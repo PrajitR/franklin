bd9250f5e74c43c1e8812f346eae8f30a982035d
Prajit Ramachandran <ramprajit@gmail.com>
2014-04-17 10:57:14 -0700

    Asks for user to refactor

--- franklin.py
+++ franklin.py
@@ -1,16 +1,16 @@
 import sys
 from random import shuffle
 
-def process_file (filename):
+def process_project (filename):
   content = ''
   with open(filename, 'r') as content_file:
     content = content_file.read()
 
-  return content.split('\n\n')
+  return content.split('def ')
 
 def get_answers (contents):
   content_pairs = []
-  print 'For each function, enter what the code does (press enter to skip)\n\n'
+  print '\n\nFor each function, enter what the code does (press enter to skip)\n\n'
   for func in contents:
     print func
     answer = raw_input('-> ')
@@ -19,7 +19,7 @@ def get_answers (contents):
     print '\n'
   return content_pairs
 
-def test (content_pairs):
+def testUnderstanding (content_pairs):
   shuffle(content_pairs)
   correct, wrong = 0, 0
 
@@ -33,16 +33,43 @@ def test (content_pairs):
       userCorrect = raw_input('Did you get it correct? (y/n) -> ')[0]
     
     if userCorrect == 'y': correct += 1
-    else : wrong += 1
+    else: wrong += 1
     
     print '\n'
 
   print ('\nYou got %d correct and %d wrong. Thats %.2f%% correct.') % \
         (correct, wrong, 100. * correct / len(content_pairs))
 
+def refactor (content_pairs):
+  shuffle(content_pairs)
+  content_trio = []
+
+  print 'Given your definition of each function, write your own version of the function\n\n'
+  for (func, answer) in content_pairs:
+    split_func = func.split('\n')
+
+    print '\n\nYour documentation: ' + answer
+    print '\nFunction skeleton:'
+    print split_func[0]
+    print '\t...'
+    print split_func[-1]
+    print '\nYour version (^D to end):\n'
+
+    refactored = sys.stdin.readlines()
+    print '\n' + func
+    content_trio.append((func, answer, '\n'.join(refactored)))
+
+  for (func, answer, refactored) in content_trio:
+    print '\n======================================'
+    print func
+    print '\n\n' + refactored
+
+  return content_trio
+
 def run ():
-  content_pairs = get_answers(process_file(sys.argv[1]))
-  test(content_pairs)
+  content_pairs = get_answers(process_project(sys.argv[1]))
+  testUnderstanding(content_pairs)
+  content_trio = refactor(content_pairs)
 
 if __name__ == '__main__':
   run()
