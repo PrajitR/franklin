4fa0e3145c936136de400b51755d96efdfbb9c73
Prajit Ramachandran <ramprajit@gmail.com>
2014-04-27 09:45:11 -0700

    Added ability to change between folders.

--- franklin.js
+++ franklin.js
@@ -9,44 +9,68 @@ var svg = d3.select('svg#chart')
     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
 var force = d3.layout.force()
-    .linkDistance(80)
-    .charge(-120)
+    .linkDistance(200)
+    .charge(function (d) { return -100 * r(d.next.length); })
     .size([width, height])
     .on('tick', tick);
+
+var r = d3.scale.linear()
+        .range([10, 40]);
 
 var link = svg.selectAll('.link'),
     node = svg.selectAll('.node');
 
-d3.json('test/example.franklin', function (error, data) {
-  var root = data['project_root'];
+var data, root, prevFolder = [];
+
+d3.select('button#back')
+    .on('click', gotoPreviousFolder);
+
+d3.json('test/example.franklin', function (error, _data) {
+  data = _data;
+  root = 'project_root';
+  update(data[root]);
+});
+
+function update(root) {
   var nodes = flatten(root),
       links = createLinks(nodes, root);
-  
+
+  r.domain([0, d3.max(nodes, function (d) { return d.next.length; })]);
   force
       .nodes(nodes)
       .links(links)
       .start();
-  console.log(links);
+
+  link.remove();
+  node.remove();
+  link = svg.selectAll('.link'),
+  node = svg.selectAll('.node');
 
   link = link.data(links);
   link.enter().insert('line', '.node')
       .attr('class', 'link');
 
-  node = node.data(nodes, function (d) { return d.id; });
+  node = node.data(nodes);
+
   var nodeEnter = node.enter().append('g')
-      .attr('class', 'node');
+      .attr('class', 'node')
+      .on('click', click);
   nodeEnter.append('circle')
-      .attr('r', 10)
+      .attr('r', function (d) { return r(d.next.length); })
+      .attr('stroke', 'black')
+      .attr('stoke-width', 2)
       .call(force.drag);
   nodeEnter.append('text')
       .attr('dy', '.35em')
+      .attr('x', function (d) { return -0.4 * r(d.next.length); })
       .text(function (d) { return d.name; });
+
   node.select("circle")
       .style("fill", function (d) {
         if (d.entry_point) return 'red';
-        else return 'black';
+        else return 'yellow';
       });
-});
+}
 
 function tick() {
   link.attr('x1', function (d) { return d.source.x; })
@@ -55,6 +79,16 @@ function tick() {
       .attr('y2', function (d) { return d.target.y; });
 
   node.attr('transform', function (d) { return 'translate(' + d.x + ',' +  d.y + ')'; });
+}
+
+function click(d) {
+  if (d3.event.defaultPrevented) return;
+  if (data[d.name]) {
+    force.stop();
+    prevFolder.push(root);
+    root = d.name;
+    update(data[root]);
+  }
 }
 
 
@@ -75,8 +109,18 @@ function createLinks(nodes, obj) {
   nodes.forEach(function (node) {
     node.next.forEach(function (f) {
       var otherNode = (nodes.filter(function (n) { return n.name == f; }))[0];
-      links.push({ source: node, target: otherNode });
+      if (otherNode)
+        links.push({ source: node, target: otherNode });
     });
   });
   return links;
+}
+
+function gotoPreviousFolder(d) {
+  var folder = prevFolder.pop();
+  if (folder) {
+    force.stop();
+    root = folder;
+    update(data[root]);
+  }
 }
