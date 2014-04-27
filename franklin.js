558a94f78f18cd9e07ff1d523836d0fee182071b
Prajit Ramachandran <ramprajit@gmail.com>
2014-04-27 11:23:32 -0700

    Changed rendering to DagreD3.

--- franklin.js
+++ franklin.js
@@ -8,19 +8,8 @@ var svg = d3.select('svg#chart')
   .append('g')
     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-var force = d3.layout.force()
-    .linkDistance(200)
-    .charge(function (d) { return -100 * r(d.next.length); })
-    .size([width, height])
-    .on('tick', tick);
-
-var r = d3.scale.linear()
-        .range([10, 40]);
-
-var link = svg.selectAll('.link'),
-    node = svg.selectAll('.node');
-
-var data, root, prevFolder = [];
+var data, root, prevFolder = [],
+    description = d3.select('p#description');
 
 d3.select('button#back')
     .on('click', gotoPreviousFolder);
@@ -28,69 +17,41 @@ d3.select('button#back')
 d3.json('test/example.franklin', function (error, _data) {
   data = _data;
   root = 'project_root';
-  update(data[root]);
+  createDagre(data[root]);
 });
 
-function update(root) {
-  var nodes = flatten(root),
-      links = createLinks(nodes, root);
+function createDagre(root) {
+  var g = new dagreD3.Digraph(),
+      nodes = flatten(root);
 
-  r.domain([0, d3.max(nodes, function (d) { return d.next.length; })]);
-  force
-      .nodes(nodes)
-      .links(links)
-      .start();
+  // Nodes
+  nodes.forEach(function (d) {
+    g.addNode(d.name, { label: d.name, description: d.description, entry_point: d.entry_point });
+  });
+  // Edges
+  nodes.forEach(function (d) {
+    d.next.forEach(function (n) {
+      if (!g.hasNode(n)) {
+        g.addNode(n, { label: n, description: '' });
+      }
+      g.addEdge(null, d.name, n);
+    });
+  });
 
-  link.remove();
-  node.remove();
-  link = svg.selectAll('.link'),
-  node = svg.selectAll('.node');
-
-  link = link.data(links);
-  link.enter().insert('line', '.node')
-      .attr('class', 'link');
-
-  node = node.data(nodes);
-
-  var nodeEnter = node.enter().append('g')
-      .attr('class', 'node')
-      .on('click', click);
-  nodeEnter.append('circle')
-      .attr('r', function (d) { return r(d.next.length); })
-      .attr('stroke', 'black')
-      .attr('stoke-width', 2)
-      .call(force.drag);
-  nodeEnter.append('text')
-      .attr('dy', '.35em')
-      .attr('x', function (d) { return -0.4 * r(d.next.length); })
-      .text(function (d) { return d.name; });
-
-  node.select("circle")
-      .style("fill", function (d) {
-        if (d.entry_point) return 'red';
-        else return 'yellow';
-      });
+  var renderer = new dagreD3.Renderer();
+  var oldDrawNodes = renderer.drawNodes();
+  renderer.drawNodes(function (graph, svg) {
+    var svgNodes = oldDrawNodes(graph, svg);
+    svgNodes.select('rect')
+        .attr('style', function (u) {
+          if (g.node(u).entry_point) return 'fill: #ffd0cd';
+        })
+        .on('click', function (u) { handleClick(g.node(u).label); })
+        .on('mouseover', function (u) { updateDescription(g.node(u).description); });
+    return svgNodes;
+  });
+  renderer.run(g, svg);
 }
-
-function tick() {
-  link.attr('x1', function (d) { return d.source.x; })
-      .attr('y1', function (d) { return d.source.y; })
-      .attr('x2', function (d) { return d.target.x; })
-      .attr('y2', function (d) { return d.target.y; });
-
-  node.attr('transform', function (d) { return 'translate(' + d.x + ',' +  d.y + ')'; });
-}
-
-function click(d) {
-  if (d3.event.defaultPrevented) return;
-  if (data[d.name]) {
-    force.stop();
-    prevFolder.push(root);
-    root = d.name;
-    update(data[root]);
-  }
-}
-
 
 function flatten(obj) {
   var nodes = [], i = 0;
@@ -103,24 +64,24 @@ function flatten(obj) {
   return nodes;
 };
 
-// The data object now has id fields thanks to flatten.
-function createLinks(nodes, obj) {
-  var links = [];
-  nodes.forEach(function (node) {
-    node.next.forEach(function (f) {
-      var otherNode = (nodes.filter(function (n) { return n.name == f; }))[0];
-      if (otherNode)
-        links.push({ source: node, target: otherNode });
-    });
-  });
-  return links;
+function handleClick(name) {
+  if (d3.event.defaultPrevented) return;
+  if (data[name]) {
+    prevFolder.push(root);
+    root = name;
+    createDagre(data[root]);
+  }
+}
+
+function updateDescription(des) {
+  description.html(des);
 }
 
 function gotoPreviousFolder(d) {
+  console.log('ay');
   var folder = prevFolder.pop();
   if (folder) {
-    force.stop();
     root = folder;
-    update(data[root]);
+    createDagre(data[root]);
   }
 }
